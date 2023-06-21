c1bba9615b7bdd18f6a2deb534cdf1d9f62976cb
JCURVEs <jaehee2173@gmail.com>
2023-06-21 20:02:09 +0900

    WaitScene 가

---
+++ Assets/Jay/Scripts/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float speed = 1f;
+    Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.material.mainTextureOffset += Vector2.up * speed * Time.deltaTime;
+    }
+}
