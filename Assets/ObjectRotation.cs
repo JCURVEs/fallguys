1be4c1b16ab0d9361ca1d85ae16f3e5521fa52eb
JCURVEs <jaehee2173@gmail.com>
2023-06-25 19:11:30 +0900

    BetaScene 초안

---
+++ Assets/ObjectRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+
+        // Convert the screen coordinates to world coordinates
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        worldPosition.z = 0f; // Set the z-coordinate to 0 to ensure it's on the same plane as the UI
+
+        // Set the position of the check mark image
+        transform.position = worldPosition;
+    }
+}
